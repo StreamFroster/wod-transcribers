a25910e5add8830644eed18b5cd6c0b7b994ad37
StreamFroster <56343624+StreamFroster@users.noreply.github.com>
2023-09-28 10:44:30 +0000

    Hide API Token, Add Logging and Minor Fixes

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import requests 
 import json
 import time
+import config
+import os
+from datetime import datetime
+
+text = '''
+ ▄█     █▄   ▄██████▄  ████████▄        ███        ▄████████    ▄████████ ███▄▄▄▄      ▄████████  ▄████████    ▄████████  ▄█  ▀█████████▄     ▄████████    ▄████████ 
+███     ███ ███    ███ ███   ▀███   ▀█████████▄   ███    ███   ███    ███ ███▀▀▀██▄   ███    ███ ███    ███   ███    ███ ███    ███    ███   ███    ███   ███    ███ 
+███     ███ ███    ███ ███    ███      ▀███▀▀██   ███    ███   ███    ███ ███   ███   ███    █▀  ███    █▀    ███    ███ ███▌   ███    ███   ███    █▀    ███    ███ 
+███     ███ ███    ███ ███    ███       ███   ▀  ▄███▄▄▄▄██▀   ███    ███ ███   ███   ███        ███         ▄███▄▄▄▄██▀ ███▌  ▄███▄▄▄██▀   ▄███▄▄▄      ▄███▄▄▄▄██▀ 
+███     ███ ███    ███ ███    ███       ███     ▀▀███▀▀▀▀▀   ▀███████████ ███   ███ ▀███████████ ███        ▀▀███▀▀▀▀▀   ███▌ ▀▀███▀▀▀██▄  ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   
+███     ███ ███    ███ ███    ███       ███     ▀███████████   ███    ███ ███   ███          ███ ███    █▄  ▀███████████ ███    ███    ██▄   ███    █▄  ▀███████████ 
+███ ▄█▄ ███ ███    ███ ███   ▄███       ███       ███    ███   ███    ███ ███   ███    ▄█    ███ ███    ███   ███    ███ ███    ███    ███   ███    ███   ███    ███ 
+ ▀███▀███▀   ▀██████▀  ████████▀       ▄████▀     ███    ███   ███    █▀   ▀█   █▀   ▄████████▀  ████████▀    ███    ███ █▀   ▄█████████▀    ██████████   ███    ███ 
+                                                  ███    ███                                                  ███    ███                                  
+                                                  ----------------------------------------------------------------------
+'''
+
+print(text)
 
 # replace with your API token
-YOUR_API_TOKEN = "111f93b614d648e5a5c984dd3b8b04b9"
+API_TOKEN = config.API_TOKEN
+print("API Token loaded!")
 
 # URL of the file to transcribe
-FILE_URL = "https://www.buzzsprout.com/1826458/13456157-2nd-anniversary.mp3?client_source=buzzsprout_site&download=true"
+FILE_URL = str(input("Enter the download link of the episode to be transcribed: "))
 
 # AssemblyAI transcript endpoint (where we submit the file)
 transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
@@ -16,26 +35,43 @@ data = {"audio_url": FILE_URL, "speaker_labels": True, "speakers_expected": 3}
 
 # HTTP request headers
 headers = {
-    "Authorization": "111f93b614d648e5a5c984dd3b8b04b9",
+    "Authorization": config.API_TOKEN,
     "Content-Type": "application/json"
 }
 
-# submit for transcription via HTTP request
+# Submit for transcription via HTTP request
 response = requests.post(transcript_endpoint, json=data, headers=headers)
 
-# polling for transcription completion
-polling_endpoint = f"https://api.assemblyai.com/v2/transcript/{response.json()['id']}"
+# Check if the request was successful
+if response.status_code != 200:
+    raise RuntimeError(f"Transcription request failed with status code {response.status_code}")
+
+# Get the transcript ID from the response
+transcript_id = response.json()['id']
+print(f"Transcription request submitted with ID: {transcript_id}")
+
+# Polling for transcription completion
+polling_endpoint = f"https://api.assemblyai.com/v2/transcript/{transcript_id}"
 
 while True:
     transcription_result = requests.get(polling_endpoint, headers=headers).json()
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     if transcription_result['status'] == 'completed':
-        # Write the results to a JSON file
-        with open("transcription_result.json", "w") as json_file:
+        # Extract the file name from the URL
+        file_name = os.path.basename(FILE_URL)
+        
+        # Create the output file name with "_transcription" appended
+        output_file_name = "./transcriptions/wip/" + os.path.splitext(file_name)[0] + "_transcription.json"
+        
+        # Write the results to the output file
+        with open(output_file_name, "w") as json_file:
             json.dump(transcription_result, json_file, indent=2)
+        
+        print(f"[{current_time}] Transcription completed. Results saved to '{output_file_name}'")
         break
-    elif transcription_result['status'] == 'error':
-        raise RuntimeError(
-            f"Transcription failed: {transcription_result['error']}")
+    elif transcription_result['status'] == 'failed':
+        raise RuntimeError(f"[{current_time}] Transcription failed: {transcription_result['error']}")
     else:
-        time.sleep(3)
+        print(f"[{current_time}] Transcription status: {transcription_result['status']}")
+        time.sleep(5)
